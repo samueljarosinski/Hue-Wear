8fd133f4dd5da8267e6bc689570cbe834a9ea888
Samuel Jarosiński <samueljarosinski@gmail.com>
2018-02-24 14:03:27 +0100

    Initial commit

---
+++ app/src/main/java/io/github/samueljarosinski/huewear/MainActivity.kt
@@ -0,0 +1,14 @@
+package io.github.samueljarosinski.huewear
+
+import android.os.Bundle
+import android.support.wearable.activity.WearableActivity
+
+class MainActivity : WearableActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+
+}
